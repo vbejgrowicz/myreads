d4642811034223c8e17a85a9f608504b420eebfe
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-07-20 19:14:19 -0400

    update style with hovering features

--- src/Components/App/Book/DisplayBook.js
+++ src/Components/App/Book/DisplayBook.js
@@ -10,7 +10,7 @@ class DisplayBook extends React.Component {
       <ol className="books-grid">
         {this.props.books.map((book, idx) => {
           return (
-            <li key={book.id + idx}>
+            <li className="full-book" key={book.id + idx}>
               <div className="book">
                 <DisplayBookTop mode={this.props.mode} book={book} booksOnShelf={this.props.booksOnShelf} updateBooks={this.props.updateBooks} searchBooks={this.props.searchBooks} />
                 <DisplayBookTitle book={book} />
