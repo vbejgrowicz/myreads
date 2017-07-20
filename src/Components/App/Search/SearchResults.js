c07a3a527296a9cf70408298c44419eb2aa8186c
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-07-20 14:24:28 -0400

    fix error when empty query returns because no search results are possible

--- src/Components/App/Search/SearchResults.js
+++ src/Components/App/Search/SearchResults.js
@@ -6,7 +6,7 @@ class SearchResults extends React.Component {
 
   render() {
     console.log(this.props.books);
-    return this.props.books ? (
+    return (this.props.books && this.props.books.length > 0) ? (
       <div className="search-books-results">
         <DisplayBook mode="search" isLoading={this.props.isLoading} books={this.props.books} booksOnShelf={this.props.booksOnShelf} updateBooks={this.props.updateBooks} />
       </div>
